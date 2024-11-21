18cfa7efcd938705e6e08c8d670d46b5c077df1c
Danilo Cassola <danilocassola@gmail.com>
2024-11-21 15:00:23 -0300

    test: add MongoDbConnection

---
+++ src/test/unit/db-connections.test.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import type { Model as ModelType } from 'mongoose';
+
+import { MongoDbConnection } from '../../infra/database/mongodb/db-connections/db-connections';
+
+describe('MongoDbConnection', () => {
+  const modelMock = {
+    save: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  } as unknown as jest.Mocked<ModelType<any>>;
+  const mongoDbConnection = new MongoDbConnection(
+    modelMock as unknown as ModelType<any>
+  );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('editar', () => {
+    it('should update a model and return the updated model', async () => {
+      const params = { id: '123', value: { nome: 'NomeProduto' } };
+      const updatedModel = { _id: '123', ...params.value };
+      modelMock.findByIdAndUpdate.mockResolvedValue(updatedModel);
+
+      const result = await mongoDbConnection.editar(params);
+
+      expect(modelMock.findByIdAndUpdate).toHaveBeenCalledWith(
+        '123',
+        params.value,
+        { new: true }
+      );
+      expect(result).toEqual(updatedModel);
+    });
+  });
+
+  describe('buscar', () => {
+    it('should find models based on params and return them', async () => {
+      const params = { categoria: 'Lanche' };
+      const foundModels = [
+        { _id: '123', nome: 'NomeProduto', categoria: 'Lanche' },
+      ];
+      modelMock.find.mockResolvedValue(foundModels);
+
+      const result = await mongoDbConnection.buscar(params);
+
+      expect(modelMock.find).toHaveBeenCalledWith(params);
+      expect(result).toEqual(foundModels);
+    });
+  });
+
+  describe('buscarUm', () => {
+    it('should find a single model based on params and return it', async () => {
+      const params = { _id: '123' };
+      const foundModel = { _id: '123', nome: 'NomeProduto' };
+      modelMock.findOne.mockResolvedValue(foundModel);
+
+      const result = await mongoDbConnection.buscarUm(params);
+
+      expect(modelMock.findOne).toHaveBeenCalledWith(params);
+      expect(result).toEqual(foundModel);
+    });
+  });
+
+  describe('excluir', () => {
+    it('should delete a model by ID', async () => {
+      const id = '123';
+      modelMock.findByIdAndDelete.mockResolvedValue(undefined);
+
+      await mongoDbConnection.excluir(id);
+
+      expect(modelMock.findByIdAndDelete).toHaveBeenCalledWith(id);
+    });
+  });
+});
