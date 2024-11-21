a295f9313a38028f79533c29d22742a9f047cbf0
Danilo Cassola <danilocassola@gmail.com>
2024-11-21 15:02:05 -0300

    test: add ProdutoDbConnection

---
+++ src/test/unit/produto-db-connection.test.ts
@@ -0,0 +1,17 @@
+import { MongoDbConnection } from '../../infra/database/mongodb/db-connections/db-connections';
+import { ProdutoDbConnection } from '../../infra/database/mongodb/db-connections/produto-db-connection';
+import { ProdutoModel } from '../../infra/database/mongodb/models';
+
+jest.mock('../../infra/database/mongodb/models');
+jest.mock('../../infra/database/mongodb/db-connections/db-connections', () => ({
+  MongoDbConnection: jest.fn(),
+}));
+
+describe('ProdutoDbConnection', () => {
+  it('should extend MongoDbConnection with ProdutoModel', () => {
+    const produtoDbConnection = new ProdutoDbConnection();
+
+    expect(MongoDbConnection).toHaveBeenCalledWith(ProdutoModel);
+    expect(produtoDbConnection).toBeInstanceOf(MongoDbConnection);
+  });
+});
